5e53bf7bd8862d5be932ae5753ea996eda9134e3
theRacterr <197937175+theRacterr@users.noreply.github.com>
2025-10-28 17:12:11 +0100

    about fragment

--- app/src/main/java/com/meowney/ui/more/AboutFragment.kt
+++ app/src/main/java/com/meowney/ui/more/AboutFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.appbar.MaterialToolbar
 import com.meowney.R
 
 class AboutFragment : Fragment() {
@@ -13,7 +17,17 @@ class AboutFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        val view = inflater.inflate(R.layout.fragment_about, container, false)
+
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.aboutToolbar)
+        val navController = findNavController()
+        toolbar.setNavigationOnClickListener {
+            navController.navigateUp()
+        }
+
+        val textView = view.findViewById<TextView>(R.id.textView)
+        textView.text = HtmlCompat.fromHtml(getString(R.string.about_text), HtmlCompat.FROM_HTML_MODE_LEGACY)
+
+        return view
     }
 }
