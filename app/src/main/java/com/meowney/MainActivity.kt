25e656d5198410e40432e23f884e0702b47a725c
theRacterr <197937175+theRacterr@users.noreply.github.com>
2025-10-28 16:20:42 +0100

    about fragment navigation

--- app/src/main/java/com/meowney/MainActivity.kt
+++ app/src/main/java/com/meowney/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.meowney
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.meowney.databinding.ActivityMainBinding
 
@@ -22,6 +22,15 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val bottomNavigationView = binding.navView
+            when (destination.id) {
+                R.id.aboutFragment -> bottomNavigationView.visibility = View.GONE
+                else -> bottomNavigationView.visibility = View.VISIBLE
+            }
+
+        }
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
