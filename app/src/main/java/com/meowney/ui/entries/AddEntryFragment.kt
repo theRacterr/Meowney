74063aabee47dd354f081f3d609ad5b474987944
theRacterr <197937175+theRacterr@users.noreply.github.com>
2025-11-12 13:53:25 +0100

    add_entry: amount input

--- app/src/main/java/com/meowney/ui/entries/AddEntryFragment.kt
+++ app/src/main/java/com/meowney/ui/entries/AddEntryFragment.kt
@@ -1,11 +1,16 @@
 package com.meowney.ui.entries
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.widget.ImageViewCompat
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.color.MaterialColors
+import com.meowney.R
 import com.meowney.databinding.FragmentAddEntryBinding
 
 
@@ -25,6 +30,24 @@ class AddEntryFragment : Fragment() {
             findNavController().navigateUp()
         }
 
+        binding.radioGroupType.setOnCheckedChangeListener { _, checkedId ->
+            var colorAttr = R.attr.toolbarElementColor
+            when (checkedId) {
+                R.id.radioIncome -> {
+                    binding.imagePosNeg.setImageResource(R.drawable.ic_plus_black_24dp)
+                    colorAttr = R.attr.profitColor
+                }
+                R.id.radioExpense -> {
+                    binding.imagePosNeg.setImageResource(R.drawable.ic_minus_black_24dp)
+                    colorAttr = R.attr.deficitColor
+                }
+                R.id.radioTransfer -> {
+                    binding.imagePosNeg.setImageResource(R.drawable.ic_tilde_black_24dp)
+                }
+            }
+            val color = MaterialColors.getColor(requireContext(), colorAttr, ContextCompat.getColor(requireContext(), R.color.black))
+            ImageViewCompat.setImageTintList(binding.imagePosNeg, ColorStateList.valueOf(color))
+        }
         return view
     }
 
