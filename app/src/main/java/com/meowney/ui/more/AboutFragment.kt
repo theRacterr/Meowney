11cadc6b33d8a27221c66ad84378f38a20ffd7b3
theRacterr <197937175+theRacterr@users.noreply.github.com>
2025-11-21 14:11:06 +0100

    styled about screen

--- app/src/main/java/com/meowney/ui/more/AboutFragment.kt
+++ app/src/main/java/com/meowney/ui/more/AboutFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.text.HtmlCompat
 import androidx.navigation.fragment.findNavController
-import com.meowney.R
 import com.meowney.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -25,9 +23,6 @@ class AboutFragment : Fragment() {
         binding.aboutToolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
         }
-
-        // passing about string as HTML for formatting
-        binding.aboutTextView.text = HtmlCompat.fromHtml(getString(R.string.about_text), HtmlCompat.FROM_HTML_MODE_LEGACY)
 
         return view
     }
