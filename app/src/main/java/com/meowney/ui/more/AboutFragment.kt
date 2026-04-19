24bdfde29309786341cd7194fa24c2f2d75147cc
theRacterr <197937175+theRacterr@users.noreply.github.com>
2026-04-20 00:20:14 +0200

    licenses button

--- app/src/main/java/com/meowney/ui/more/AboutFragment.kt
+++ app/src/main/java/com/meowney/ui/more/AboutFragment.kt
@@ -1,11 +1,13 @@
 package com.meowney.ui.more
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.oss.licenses.v2.OssLicensesMenuActivity
 import com.meowney.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
@@ -22,6 +24,10 @@ class AboutFragment : Fragment() {
 
         binding.aboutToolbar.setNavigationOnClickListener {
             findNavController().popBackStack()
+        }
+
+        binding.licensesButton.setOnClickListener {
+            startActivity(Intent(requireContext(), OssLicensesMenuActivity::class.java))
         }
 
         return view
